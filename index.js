5791993c5957d5a49511f8eba65275fab7ef59c8
Vitalii <vetal.shtager@gmail.com>
2022-04-07 18:45:37 -0400

    commit tree 9c187640eed76ec8fa50facef59b1c28cc421453

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+
+
+
+function findMatching(drivers, name) {
+    let driversNames = drivers.filter( function(matchingName) {
+        return matchingName === name || matchingName === name.toLowerCase() || matchingName === name.toUpperCase
+    })
+    return driversNames
+}
+
+function fuzzyMatch(drivers, name) {
+    let beginningLettersMatch = drivers.filter( function(firstLetters) {
+        return firstLetters.slice(0, name.length) === name 
+    })
+    return beginningLettersMatch
+}
+
+function matchName(drivers, name) {
+    let matchingNames = drivers.filter( function(driver) {
+        return driver['name'] === name
+    })
+    return matchingNames
+}
